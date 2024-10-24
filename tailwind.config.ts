a9c63978ee0dc7397d9109f9a6720b07ba51ad11
zadrach caunca <zad@seabudigital.com>
2024-10-25 00:15:52 +0800

    Update dependencies and font styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
